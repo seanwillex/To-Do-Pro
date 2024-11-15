4514ed38eff7927214be42b0ed8dfefa0234a835
seanwillex <seanwillex@gmail.com>
2024-11-15 23:45:03 +0100

    Update WellnessTracker.tsx

--- src/components/personal-development/wellness/WellnessTracker.tsx
+++ src/components/personal-development/wellness/WellnessTracker.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Task, Habit, TimeEntry, WellnessLog, WellnessMetrics, WellnessActivities } from '@/types';
 import {
   Card,
